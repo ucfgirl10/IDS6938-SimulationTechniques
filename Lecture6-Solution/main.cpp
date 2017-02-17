50c3405749de3ffadae10973fb6952981c1f79cd
Karla Badillo <kcurquiola10@knights.ucf.edu>
2017-02-16 19:26:18 -0500

    NOWWWWW check my graphs :)

--- Lecture6-Solution/main.cpp
+++ Lecture6-Solution/main.cpp
@@ -13,7 +13,7 @@ Compute the dy/dx
 */
 double df(double x, double y)            //function for defining dy/dx
 {
-    return y - (1 / 2)*exp(x / 2)*sin(5 * x) + 5 * exp(x / 2)*cos(5 * x);
+    return y - (1.0 / 2.0)*pow(M_E,(x / 2.0))*sin(5.0 * x) + 5 * pow(M_E,(x / 2.0))*cos(5.0 * x);
 }
 
 
@@ -22,7 +22,7 @@ Compute the exact answer
 */
 double exact(double x)            //function for defining dy/dx
 {
-    return exp(x / 2)*sin(5 * x);
+    return pow(M_E,(x / 2.0))*sin(5.0 * x);
 }
 
 
@@ -92,32 +92,35 @@ std::string toString(double x, double y_euler, double y_midpoint, double y_RK4, 
 int main()
 {
 
-    double y_euler = 0.0;
+	double y_euler = 0.0;
 	double y_midpoint = 0.0;
 	double y_RK4 = 0.0;
-    double x = 0.0;
-    double h = 0.1;
+	double x = 0.0;
+	double xb = 10.0; //addition
+	double h = 0.5;
+
+	int range = (xb - x) / h; //addition
 
 	std::cout << std::fixed << std::showpoint;
-    // Header information for column printouts
+	// Header information for column printouts
 	std::cout << "x" << "\t" << "y (Euler)" << "\t" << "y (midpoint)" << "\t" << "y (RK44)" << "\t"
 		<< "EXACT" << "\t\t" << "%Err(E)" << "\t\t" << "%Err(M)" << "\t\t" << "%Err(RK4)" << std::endl;
 	std::cout << "----" << "\t" << "----------" << "\t" << "----------" << "\t" << "----------"
-		<< "\t" << "----------" << "\t" << "-------"<<"\t\t" << "-------" << "\t\t" << "------" << std::endl;
+		<< "\t" << "----------" << "\t" << "-------" << "\t\t" << "-------" << "\t\t" << "------" << std::endl;
 
 	//intial values
-	std::cout<< toString(x, y_euler, y_midpoint, y_RK4, exact(x), 10) << std::endl;
+	std::cout << toString(x, y_euler, y_midpoint, y_RK4, exact(x), 10) << std::endl;
 
-    for (int i = 0; i < 50; i++)
-    {
-		y_euler = rk1(y_euler,h,x);  //caculate y_{i+1}
+	for (int i = 0; i < range; i++)
+	{
+		y_euler = rk1(y_euler, h, x);  //caculate y_{i+1}
 		y_midpoint = rk2(y_midpoint, h, x);  //caculate y_{i+1}
 		y_RK4 = rk4(y_RK4, h, x);  //caculate y_{i+1}
-        x = x + h;       //increment x
+		x = x + h;       //increment x
 
 		std::cout << toString(x, y_euler, y_midpoint, y_RK4, exact(x), 10) << std::endl;
 	}
 
 
-    return 0;
+	return 0;
 }
